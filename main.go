1a0e75a2413f4e55f59afa36321ea68f0e3a1f34
Slach <bloodjazman@gmail.com>
2022-05-25 15:45:58 +0400

    Bump version: 2021.1.0 → 2021.2.0

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	app.Usage = "visualize clickhouse system.trace_log as flamegraph, based on https://gist.github.com/alexey-milovidov/92758583dd41c24c360fdb8d6a4da194"
 	app.ArgsUsage = ""
 	app.HideHelp = false
-	app.Version = "2021.1.0"
+	app.Version = "2021.2.0"
 	app.Flags = []cli.Flag{
 		&cli.IntFlag{
 			Name:  "width",
